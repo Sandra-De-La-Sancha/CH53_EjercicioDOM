c1a6d269f7c2011e69244fd4be0511bf9c224e7b
sandy <sandra.de.la.sancha.aguilar.isc.2000@gmail.com>
2025-04-08 12:24:42 -0600

    Se agregan funcionalidades al main

--- JS/main.js
+++ JS/main.js
@@ -1,28 +1,41 @@
-//DOCUMENT es la parte donde se puede acceder a todo el 
-//html
+let encabezado1 = document.getElementById("encabezado1");
+let encabezado2 = document.getElementById("encabezado2");
+let lista = document.getElementsByTagName("ul");
+let elementos = document.getElementsByClassName("list-group-item");
+let otroElemento = document.querySelector("ul>li");
+let otrosElementos = document.querySelectorAll("ul>li");
+let btnMostrar = document.getElementById("btnMostrar");
 
-//Accediendo a la etiqueta h1 con el id
-//console.log(document.getElementById("encabezado1"));
-let btnMostrar =document.getElementById("btnMostrar")
-let encabezado1 =document.getElementById("encabezado1");
-let encabezado2 =document.getElementById("encabezado2");
-let lista= document.getElementsByTagName("ul");
+let contador = 0;
 
-let contador=0;
+console.log(lista.length);
+console.log(lista[0]);
+console.log(lista.item(1))
+console.log(elementos.length);
+console.log(elementos.item(2));
+console.log("otroElemento:", otroElemento);
+console.log("otrosElementos:", otrosElementos.length);
+
 function modifica(){
+    encabezado1.innerText = "Ejercicio DOM";
+    encabezado2.innerHTML = "<em> DOM Exercise <em>";
+}
+
+console.log(encabezado1.innerText);
+console.log(encabezado2.innerText);
+
+btnMostrar.addEventListener("click", function(event){
+    event.preventDefault(); //no hagas lo que haces por defecto
+   // console.log("Botón btnModificar presionado");
+
+    let element = document.createElement("li");
+    element.innerText = "Another item"; //<li> Another item </li>
+    element.classList.add("list-group-item");
+
+    let element2 = element.cloneNode(true);
+
+    listas.item(0).before(element);
+    listas.item(0).prepend(element);
     
-    encabezado1.innerHTML +="<em>Ejercicio</em> DOM";
-    encabezado2.innerText = ++contador;
-}// modifica
-
-btnMostrar.addEventListener("click", function(event) {
-    event.preventDefault();//no hagas lo quehaces por defecto
-
-
-
+    
 });
-//console.log(encabezado1.innerText); //es uns propiedad de la etiqueta que esta almacenando el texto de adentro
-
-
-//encabezado2.innerText = "Ejercicio 2 DOM";
-//console.log(encabezado2.innerText);
