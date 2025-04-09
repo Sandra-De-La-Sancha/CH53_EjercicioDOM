6334ca7a294187fb237edf8b0f519f86e93087cd
sandy <sandra.de.la.sancha.aguilar.isc.2000@gmail.com>
2025-04-08 23:31:14 -0600

    Se agregan lineas faltantes del main

--- JS/main.js
+++ JS/main.js
@@ -1,33 +1,41 @@
+let btnMostrar = document.getElementById("btnMostrar");
 let encabezado1 = document.getElementById("encabezado1");
 let encabezado2 = document.getElementById("encabezado2");
-let lista = document.getElementsByTagName("ul");
-let elementos = document.getElementsByClassName("list-group-item");
-let otroElemento = document.querySelector("ul>li");
-let otrosElementos = document.querySelectorAll("ul>li");
-let btnMostrar = document.getElementById("btnMostrar");
+let listas = document.getElementsByTagName("ul");
 
+
+let elementos = document.getElementsByClassName("list-group-item");
+
+let otroElemento = document.querySelector("ul>li");//primero
+
+let otrosElementos = document.querySelectorAll("ul>li");//todos
+
+console.log(otrosElementos.length);
+
+console.log("otroElemento:", otroElemento);
+
+console.log(listas.length);//2 (longitudes)
+
+//console.log(lista[0]);
+console.log(listas.item(1));
+
+console.log(elementos.item(2));
+
+console.log(elementos.length);
 let contador = 0;
 
-console.log(lista.length);
-console.log(lista[0]);
-console.log(lista.item(1))
-console.log(elementos.length);
-console.log(elementos.item(2));
-console.log("otroElemento:", otroElemento);
-console.log("otrosElementos:", otrosElementos.length);
+//console.log("otrosElementos:", otrosElementos.length);
 
 function modifica(){
-    encabezado1.innerText = "Ejercicio DOM";
-    encabezado2.innerHTML = "<em> DOM Exercise <em>";
+   // encabezado1.innerText = "Ejercicio DOM";
+    encabezado1.innerHTML += "<em> Ejercicio </em> DOM";
+    encabezado2.innerText = ++contador;
 }
 
-console.log(encabezado1.innerText);
-console.log(encabezado2.innerText);
-
+//console.log(encabezado1.innerText);
 btnMostrar.addEventListener("click", function(event){
     event.preventDefault(); //no hagas lo que haces por defecto
-   // console.log("Botón btnModificar presionado");
-
+   
     let element = document.createElement("li");
     element.innerText = "Another item"; //<li> Another item </li>
     element.classList.add("list-group-item");
